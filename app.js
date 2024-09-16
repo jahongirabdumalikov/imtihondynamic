af1ccf74c8b68005ee15ab3daa3920995381f026
jahongirabdumalikov <abdumalikovj015@gmail.com>
2024-09-16 06:30:18 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// let str = "salom";
+// console.log(str.toUpperCase());
+
+// let str = "JavaScript"
+// console.log(str.slice(0, 4));
+
+
+// let str = "Olma, Banan, Gilos";
+// let fruits=[str];
+// console.log(fruits);
+
+// let str = "Salom Dunyo!";
+// let newStr = str.replace("Dunyo", "Olam")
+// console.log(newStr);
+
+
+// let num1 = 10;
+// let num2 = 20;
+// let num3 = 30;
+// let num4 = 40;
+// let natija = num1 + num2 + num3 +num4 
+// console.log(natija);
+
+
+
+// let ism = "Ali";
+// let yosh = 25;
+// let manzil = "Toshkent";
+// let kasb = "Dasturchi";
+// let natija = 'Ali, ism:25 yosh, Manzil:Toshkent, Kasb: Dasturchi'
+// console.log(natija);
+
+// let sonlar = [10, 20, 30, 40, 50];
+
+// console.log("Arraydagi birinchi son: " + sonlar[0]);
+// console.log("Arraydagi ikkinchi son: " + sonlar[1]);
+// console.log("Arraydagi uchinchi son: " + sonlar[2]);
+
+
+// let yosh = 20;
+// let ism = "Olim";
+// let name = 'Talabani ismi:' + ism
+// let age = 'Talabaning yoshi:' + yosh
+// let fanlar = ["Matematika", "Fizika", "Ingliz tili"];
+// let natija = 'oqiyotgan fanlar:' + fanlar[0] + fanlar[1] + fanlar[2];
+
+// console.log(age);
+// console.log(name);
+// console.log(natija);
